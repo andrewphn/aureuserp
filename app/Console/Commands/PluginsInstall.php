5145c456f2eb8f22b941c00eb3e454c0c53a14e3
andrewphn <131307992+andrewphn@users.noreply.github.com>
2025-10-08 19:44:26 -0400

    refactor: rename install:plugins to plugins:install
    Changed command signature from 'install:plugins' to 'plugins:install'
    to match Laravel/ERP naming convention where commands are grouped
    by resource (e.g., erp:install, products:install, plugins:install).
    
    Updated:
    - File renamed: InstallPlugins.php → PluginsInstall.php
    - Class renamed: InstallPlugins → PluginsInstall
    - Command signature: install:plugins → plugins:install
    
    Usage:
      php artisan erp:install      # Install core ERP
      php artisan plugins:install  # Install all plugins

--- app/Console/Commands/PluginsInstall.php
+++ app/Console/Commands/PluginsInstall.php
@@ -4,14 +4,14 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 
-class InstallPlugins extends Command
+class PluginsInstall extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'install:plugins';
+    protected $signature = 'plugins:install';
 
     /**
      * The console command description.
