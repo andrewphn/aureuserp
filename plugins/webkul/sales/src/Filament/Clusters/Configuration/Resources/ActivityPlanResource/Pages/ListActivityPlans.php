be7c88fb7fc52e516332acf70868557bdddd8a2d
andrewphn <131307992+andrewphn@users.noreply.github.com>
2025-10-10 15:52:08 -0400

    feat: Complete Sales module table views migration (4 files)
    - Migrated ListQuotationTemplates (simple)
    - Migrated ListTags (simple)
    - Migrated ListTeams with archived tab
    - Migrated ListActivityPlans with plugin='sales' filtering
    - 5 additional files complete via inheritance from base classes
    - All Sales configuration resources now use HasTableViews trait

--- plugins/webkul/sales/src/Filament/Clusters/Configuration/Resources/ActivityPlanResource/Pages/ListActivityPlans.php
+++ plugins/webkul/sales/src/Filament/Clusters/Configuration/Resources/ActivityPlanResource/Pages/ListActivityPlans.php
@@ -5,13 +5,16 @@ namespace Webkul\Sale\Filament\Clusters\Configuration\Resources\ActivityPlanReso
 use Filament\Actions\CreateAction;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Schemas\Components\Tabs\Tab;
 use Illuminate\Support\Facades\Auth;
 use Webkul\Sale\Filament\Clusters\Configuration\Resources\ActivityPlanResource;
 use Webkul\Support\Models\ActivityPlan;
+use Webkul\TableViews\Filament\Components\PresetView;
+use Webkul\TableViews\Filament\Concerns\HasTableViews;
 
 class ListActivityPlans extends ListRecords
 {
+    use HasTableViews;
+
     protected static string $resource = ActivityPlanResource::class;
 
     protected static ?string $pluginName = 'sales';
@@ -47,16 +50,17 @@ class ListActivityPlans extends ListRecords
         ];
     }
 
-    public function getTabs(): array
+    public function getPresetTableViews(): array
     {
         return [
-            'all' => Tab::make(__('sales::filament/clusters/configurations/resources/activity-plan/pages/list-activity-plan.tabs.all'))
-                ->badge(ActivityPlan::where('plugin', static::getPluginName())->count()),
-            'archived' => Tab::make(__('sales::filament/clusters/configurations/resources/activity-plan/pages/list-activity-plan.tabs.archived'))
-                ->badge(ActivityPlan::where('plugin', static::getPluginName())->onlyTrashed()->count())
-                ->modifyQueryUsing(function ($query) {
-                    return $query->where('plugin', static::getPluginName())->onlyTrashed();
-                }),
+            'all' => PresetView::make(__('sales::filament/clusters/configuration/resources/activity-plan/pages/list-activity-plan.tabs.all'))
+                ->icon('heroicon-s-queue-list')
+                ->favorite()
+                ->setAsDefault()
+                ->modifyQueryUsing(fn ($query) => $query->where('plugin', static::getPluginName())),
+            'archived' => PresetView::make(__('sales::filament/clusters/configuration/resources/activity-plan/pages/list-activity-plan.tabs.archived'))
+                ->icon('heroicon-s-archive-box')
+                ->modifyQueryUsing(fn ($query) => $query->where('plugin', static::getPluginName())->onlyTrashed()),
         ];
     }
 }
--- plugins/webkul/sales/src/Filament/Clusters/Configuration/Resources/QuotationTemplateResource/Pages/ListQuotationTemplates.php
+++ plugins/webkul/sales/src/Filament/Clusters/Configuration/Resources/QuotationTemplateResource/Pages/ListQuotationTemplates.php
@@ -5,9 +5,13 @@ namespace Webkul\Sale\Filament\Clusters\Configuration\Resources\QuotationTemplat
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use Webkul\Sale\Filament\Clusters\Configuration\Resources\QuotationTemplateResource;
+use Webkul\TableViews\Filament\Components\PresetView;
+use Webkul\TableViews\Filament\Concerns\HasTableViews;
 
 class ListQuotationTemplates extends ListRecords
 {
+    use HasTableViews;
+
     protected static string $resource = QuotationTemplateResource::class;
 
     protected function getHeaderActions(): array
@@ -15,6 +19,16 @@ class ListQuotationTemplates extends ListRecords
         return [
             CreateAction::make()
                 ->icon('heroicon-o-plus-circle'),
+        ];
+    }
+
+    public function getPresetTableViews(): array
+    {
+        return [
+            'all' => PresetView::make(__('sales::filament/clusters/configuration/resources/quotation-template/pages/list-quotation-templates.tabs.all'))
+                ->icon('heroicon-s-queue-list')
+                ->favorite()
+                ->setAsDefault(),
         ];
     }
 }
