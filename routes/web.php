469d22a2da5c4db4290cdcdf391a4ede64dfdbcf
andrewphn <131307992+andrewphn@users.noreply.github.com>
2025-10-16 11:41:16 -0400

    fix: remove duplicate legacy PDF annotation routes from web.php
    - Removed old PdfAnnotationController routes that were causing 500 errors
    - All PDF annotation routes now properly defined in routes/api.php
    - Uses new App\Http\Controllers\Api\PdfAnnotationController
    - Fixes: Call to undefined method getPageAnnotations() error

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PdfPreviewController;
-use App\Http\Controllers\PdfAnnotationController;
 
 if (! request()->getRequestUri() == '/login') {
     Route::redirect('/login', '/admin/login')
@@ -15,24 +14,5 @@ Route::get('/api/pdf/{pdfId}/page/{pageNumber}/render', [PdfPreviewController::c
 Route::get('/api/pdf/{pdfId}/page/{pageNumber}/render-base64', [PdfPreviewController::class, 'renderPageBase64'])
     ->name('pdf.render.page.base64');
 
-// PDF Annotation Routes
-Route::prefix('api/pdf/annotations')->group(function () {
-    Route::get('/page/{pdfPageId}', [PdfAnnotationController::class, 'getPageAnnotations'])
-        ->name('pdf.annotations.get');
-    Route::post('/page/{pdfPageId}', [PdfAnnotationController::class, 'savePageAnnotations'])
-        ->name('pdf.annotations.save');
-    Route::post('/page/{pdfPageId}/create', [PdfAnnotationController::class, 'createAnnotation'])
-        ->name('pdf.annotations.create');
-    Route::patch('/{annotationId}/link', [PdfAnnotationController::class, 'linkAnnotation'])
-        ->name('pdf.annotations.link');
-    Route::delete('/{annotationId}', [PdfAnnotationController::class, 'deleteAnnotation'])
-        ->name('pdf.annotations.delete');
-    Route::get('/page/{pdfPageId}/cabinet-runs', [PdfAnnotationController::class, 'getAvailableCabinetRuns'])
-        ->name('pdf.annotations.cabinet-runs');
-    Route::get('/cabinet-run/{cabinetRunId}/cabinets', [PdfAnnotationController::class, 'getCabinetsInRun'])
-        ->name('pdf.annotations.run-cabinets');
-
-    // Save entire annotated PDF using Nutrient Processor API
-    Route::post('/document/{pdfId}/save', [PdfAnnotationController::class, 'saveAnnotatedPdf'])
-        ->name('pdf.annotations.save-document');
-});
+// NOTE: PDF Annotation API routes have been moved to routes/api.php
+// using the new App\Http\Controllers\Api\PdfAnnotationController
