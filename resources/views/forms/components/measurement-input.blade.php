b727fc9e4fa562d10e7be15cef118aeaf348722f
andrewphn <131307992+andrewphn@users.noreply.github.com>
2026-01-12 18:35:29 -0500

    fix: Output PHP variable directly in x-model for unit selector
    - Change :x-model to x-model with PHP output
    - Fixes 'not defined' error for unit selector binding

--- resources/views/forms/components/measurement-input.blade.php
+++ resources/views/forms/components/measurement-input.blade.php
@@ -42,7 +42,7 @@
                     class="fi-fo-select"
                 >
                     <select
-                        :x-model="$alpineUnitSelectorPath"
+                        x-model="{{ $alpineUnitSelectorPath }}"
                         @if($isDisabled()) disabled @endif
                         class="fi-input block w-full border-none bg-transparent px-3 py-1.5 pr-8 text-base text-gray-950 outline-none transition duration-75 placeholder:text-gray-400 focus:ring-0 disabled:text-gray-500 disabled:[-webkit-text-fill-color:theme(colors.gray.500)] disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.400)] sm:text-sm sm:leading-6 dark:text-white dark:placeholder:text-gray-500 dark:disabled:text-gray-400 dark:disabled:[-webkit-text-fill-color:theme(colors.gray.400)] dark:disabled:placeholder:[-webkit-text-fill-color:theme(colors.gray.500)] appearance-none cursor-pointer"
                     >
